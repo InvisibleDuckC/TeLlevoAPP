dd78cc32fe6b0184608a9374497c9ab416f86a9f
LuHermosilla <166053578+LuHermosilla@users.noreply.github.com>
2024-11-13 21:51:45 -0300

    Instalar Jasmine
    se corrigen errores

--- src/app/login/login.page.spec.ts
+++ src/app/login/login.page.spec.ts
@@ -1,11 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginPage } from './login.page';
+import { IonicModule } from '@ionic/angular';
+import { FirestoreService } from '../services/firestore.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LoginPage],
+      imports: [IonicModule.forRoot(), RouterTestingModule], // Asegúrate de incluir RouterTestingModule
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            // Mock para valores necesarios en las pruebas
+            params: of({}), // Simular parámetros según sea necesario
+            queryParams: of({})
+          }
+        },
+        {
+          provide: FirestoreService,
+          useValue: {
+            // Mock de FirestoreService
+            userExists: jasmine.createSpy('userExists').and.returnValue(Promise.resolve(false)),
+            getUserData: jasmine.createSpy('getUserData').and.returnValue(Promise.resolve(null)),
+            addUser: jasmine.createSpy('addUser').and.returnValue(Promise.resolve()),
+            updateUser: jasmine.createSpy('updateUser').and.returnValue(Promise.resolve())
+          }
+        }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/profile/profile.page.spec.ts
+++ src/app/profile/profile.page.spec.ts
@@ -1,16 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfilePage } from './profile.page';
+import { IonicModule } from '@ionic/angular';
+import { FirestoreService } from '../services/firestore.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('ProfilePage', () => {
   let component: ProfilePage;
   let fixture: ComponentFixture<ProfilePage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ProfilePage],
+      imports: [IonicModule.forRoot(), RouterTestingModule], // Asegúrate de incluir RouterTestingModule
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            // Mock para valores necesarios en las pruebas
+            params: of({}), // Simular parámetros según sea necesario
+            queryParams: of({})
+          }
+        },
+        {
+          provide: FirestoreService,
+          useValue: {
+            // Mock de FirestoreService
+            userExists: jasmine.createSpy('userExists').and.returnValue(Promise.resolve(false)),
+            getUserData: jasmine.createSpy('getUserData').and.returnValue(Promise.resolve(null)),
+            addUser: jasmine.createSpy('addUser').and.returnValue(Promise.resolve()),
+            updateUser: jasmine.createSpy('updateUser').and.returnValue(Promise.resolve())
+          }
+        }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ProfilePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
